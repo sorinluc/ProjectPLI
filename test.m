a5c9e12731c9818e33b8b5a06b040e7c570b522a
LeLu <sorin.luc@gmail.com>
2016-12-04 19:16:19 -0300

    On continue

--- test.m
+++ test.m
@@ -18,24 +18,45 @@ nb = B./sqrt(R.^2 + G.^2 + B.^2);
 
 fimg = cat(3,nr,ng,nb);
 
-
-imshowpair(img, fimg, 'montage');
+%imshowpair(img, fimg, 'montage');
 
 
 rows = size(fimg, 1);
 cols = size(fimg, 2);
 
 
-cov = eye(3, 3);
+cov = [1,0,0;0,1,0;0,0,1];
 bin_img = zeros(rows, cols);
 
 
+%for i = 1:rows
+ %   for j = 1:cols
+  %      c = [fimg(i,j,1), fimg(i,j,2), fimg(i,j,3)];
+   %     lambda_s = c * cov * c';
+    %    if lambda_s <= t
+     %      bin_img(i, j) = 1; 
+      %  end
+ %   end
+%end
+
+[~,bin_img] = generate_skinmap('tete2.jpg');
+
+L = bwlabel(bin_img, 4);
+
+reverseL = L;
+
+rows = size(L, 1);
+cols = size(L, 2);
+
 for i = 1:rows
     for j = 1:cols
-        lambda_s = (fimg(i,j,:) * cov * fimg(i,j,:)');
-        if lambda_s <= t
-           bin_img(i, j) = 1; 
+        if L(i,j) == 0
+            reverseL(i,j) = 1;
+        else
+            reverseL(i,j) = 0;
         end
     end
 end
-L = bwlabel(bin_img);
+
+imshow(reverseL)
+
